d144d8187b1fb0ba89006817bce1df0b309a0819
Muhamad Hilmy Haidar <hilmy_21@apps.ipb.ac.id>
2020-08-14 10:22:57 +0700

    using env for email and password

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const port = 6789;
@@ -7,8 +9,9 @@ app.get("/", (req, res) => {
 	res.json({ message: "I am alive" });
 });
 app.post("/webhook", (req, res) => {
+	const origin = `https://${process.env.USERNAME}:${process.env.PASSWORD}@github.com/mhilmyh/website-psn.git master`;
 	exec(
-		"cd /var/www/psn && git fetch origin && git reset --hard origin/master && npm install && npm run build",
+		`cd /var/www/psn && git fetch ${origin} && git reset --hard ${origin} && npm install && npm run build`,
 		(error, stdout, stderr) => {
 			if (error) {
 				res.status(500).json({ error, stdout, stderr });

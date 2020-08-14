a45a3e9372622fe406a2f9d2feac2df8ede5ff03
Muhamad Hilmy Haidar <hilmy_21@apps.ipb.ac.id>
2020-08-14 11:18:19 +0700

    use req params

--- index.js
+++ index.js
@@ -9,17 +9,25 @@ app.get("/", (req, res) => {
 	res.json({ message: "I am alive" });
 });
 app.post("/webhook", (req, res) => {
-	const origin = `https://${process.env.USERNAME}:${process.env.PASSWORD}@github.com/mhilmyh/website-psn.git master`;
-	exec(
-		`cd /var/www/psn && git pull -f ${origin} && npm install && npm run build`,
-		(error, stdout, stderr) => {
-			if (error) {
-				res.status(500).json({ error: error.code, stdout, stderr });
-				return;
-			}
-			res.status(200).json({ error: error.code, stdout, stderr });
+	const payload = {
+		username: req.params.username ? req.params.username : process.env.USERNAME, // better use username from params
+		username: req.params.password ? req.params.password : process.env.PASSWORD, // better use password from params
+		name: req.params.name ? req.params.name : process.env.NAME,
+		repo: req.params.repository
+			? req.params.repository
+			: process.env.REPOSITORY,
+		branch: req.params.branch ? req.params.branch : process.env.BRANCH,
+	};
+	const origin = `https://${payload.username}:${payload.password}@github.com/${payload.name}/${payload.repo}.git ${payload.branch}`;
+	const escapedOrigin = String(origin).replace(/([\"\'\$\`\\])/g, "\\$1");
+	const cmd = `cd /var/www/psn && git pull -f ${String(escapedOrigin)}`;
+	exec(cmd, (error, stdout, stderr) => {
+		if (error) {
+			res.status(500).json({ error: error.code, stdout, stderr });
+			return;
 		}
-	);
+		res.status(200).json({ error: error.code, stdout, stderr });
+	});
 });
 
 app.listen(port, () => console.log(`Running server in port ${port}`));

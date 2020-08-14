b42a4e0f6de6f2fa3db80bb6de8a37abc2338514
Muhamad Hilmy Haidar <hilmy_21@apps.ipb.ac.id>
2020-08-14 13:51:35 +0700

    change params to query

--- index.js
+++ index.js
@@ -10,14 +10,12 @@ app.get("/", (req, res) => {
 });
 app.post("/webhook", (req, res) => {
 	const payload = {
-		workdir: req.params.workdir ? req.params.workdir : process.env.WORKDIR,
-		username: req.params.username ? req.params.username : process.env.USERNAME, // better use username from params
-		password: req.params.password ? req.params.password : process.env.PASSWORD, // better use password from params
-		name: req.params.name ? req.params.name : process.env.NAME,
-		repo: req.params.repository
-			? req.params.repository
-			: process.env.REPOSITORY,
-		branch: req.params.branch ? req.params.branch : process.env.BRANCH,
+		workdir: req.query.workdir ? req.query.workdir : process.env.WORKDIR,
+		username: req.query.username ? req.query.username : process.env.USERNAME, // better use username from query
+		password: req.query.password ? req.query.password : process.env.PASSWORD, // better use password from query
+		name: req.query.name ? req.query.name : process.env.NAME,
+		repo: req.query.repository ? req.query.repository : process.env.REPOSITORY,
+		branch: req.query.branch ? req.query.branch : process.env.BRANCH,
 	};
 	const origin = `https://${payload.username}:${payload.password}@github.com/${payload.name}/${payload.repo}.git ${payload.branch}`;
 	const escapedOrigin = String(origin).replace(/([\"\'\$\`\\])/g, "\\$1");

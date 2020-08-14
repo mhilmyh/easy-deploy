b313c37c5d885ccb0ca77b8c4957e364d7773a3a
Muhamad Hilmy Haidar <hilmy_21@apps.ipb.ac.id>
2020-08-14 08:49:18 +0700

    deploy for web psn

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const port = 6789;
+const { spawn } = require("child_process");
+app.post("/webhook", (req, res) => {
+	const deploy = spawn(
+		"git fetch origin && git reset --hard origin/master && npm install && npm run build",
+		{
+			cwd: "/var/www/psn/",
+			shell: true,
+		}
+	);
+
+	deploy.stdout.on("data", (data) => {
+		console.log(`stdout: ${data}`);
+	});
+
+	deploy.stderr.on("data", (data) => {
+		console.error(`stderr: ${data}`);
+	});
+
+	deploy.on("close", (code) => {
+		console.log(`child process exited with code ${code}`);
+	});
+});
+
+app.listen(port, () => console.log(`Running server in port ${port}`));
